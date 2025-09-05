580b847e668f0955533a79ef75ff0c5e324a9216
tndreka <tndreka@student.42heilbronn.de>
2025-09-05 17:40:54 +0200

    ADD: following the rules of templates i have defined the first template function swap that swaps two values

--- ex00/Template.hpp
+++ ex00/Template.hpp
@@ -6,12 +6,23 @@
 /*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/05 17:10:36 by tndreka           #+#    #+#             */
-/*   Updated: 2025/09/05 17:12:57 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/09/05 17:38:36 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TEMPLATES_HPP
 #define TEMPLATES_HPP
+
+#include <iostream>
+
+template<typename T>
+void swap(T &a, T &b)
+{
+	T tmp;
+	tmp = a;
+	a = b;
+	b = tmp;
+}
 
 
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,10 +6,11 @@
 /*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/05 17:01:24 by tndreka           #+#    #+#             */
-/*   Updated: 2025/09/05 17:07:01 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/09/05 17:38:56 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Template.hpp"
 
 int main( void )
 {
@@ -17,13 +18,13 @@ int main( void )
 	int b = 3;
 	::swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	// std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	// std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 	std::string c = "chaine1";
 	std::string d = "chaine2";
 	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	// std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	// std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 	return 0;
 	}
