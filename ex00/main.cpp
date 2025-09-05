873700b6d7e7f24cbffd375e06767658b1e8c5aa
tndreka <tndreka@student.42heilbronn.de>
2025-09-05 17:14:35 +0200

    ADD: added the main.cpp from the subject created a template header file in which there i will create the  swapm min and max.

---
+++ ex00/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/05 17:01:24 by tndreka           #+#    #+#             */
+/*   Updated: 2025/09/05 17:07:01 by tndreka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+int main( void )
+{
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+	}
