10cb0d4ac924de7fd2c6245d5d2201df2159618b
Taulant Ndreka <tndreka@3-G-7.42heilbronn.de>
2025-09-16 13:34:10 +0200

    ADD: created the base of the iter template

--- ex01/Iter.hpp
+++ ex01/Iter.hpp
@@ -13,4 +13,33 @@
 #ifndef ITER_HPP
 #define ITER_HPP
 
+#include <iostream>
+
+template<typename T, typename Func>
+
+/*
+    iter(array, lenth[array], function)
+    => Iter function takes three parameter
+    1) Address of array
+    2) Size of array
+    3) Function applied to every element on array
+*/
+
+/*
+    <typename Func> => this is a template parameter called func
+    it can be any datatype.
+    when the iter() is getting called the third parameter Func
+    is automatically known by the compiler.
+    you can pass a function to this and the third parameter Func can be a function 
+    or a lambda or a functor etc. so the compile figures out the actual type when the function 
+    will be called 
+*/
+void    iter(T *array, size_t len, Func func)
+{
+    for (size_t i = 0; i < len; i++)
+    {
+    }
+    
+}
+
 #endif
